d522e0ca0dd0a5fc7a4d3b41024358f98a26dc42
eduardo costa <educostabra@gmail.com>
2019-09-06 15:13:50 -0300

    1# Solution

--- Home/house_password.py
+++ Home/house_password.py
@@ -3,14 +3,23 @@
 # 
 # END_DESC
 
+import re
+# https://py.checkio.org/en/mission/house-password/
+# Input: A password as string with letters and digits
+# Output: Is the password safe or not as boolean
+# Conditions: A password is safe when:
+# (1) lengh is greater than or equal to 10
+# (2) has at leat one digit
+# (3) has at least one uppercase letter
+# (4) has at least one lowercase letter
 def checkio(data: str) -> bool:
-
-    #replace this for solution
-    return True or False
-
-#Some hints
-#Just check all conditions
-
+    # Check if data lenght is >= 10 and
+    # Check if data has a digit and
+    # Check if data has a uppercase And
+    # Check if data has lower case
+    # The password is strong then return true otherwise false.
+    return len(data) >= 10 and re.search("[0-9]", data) != None and re.search("[a-z]", data) != None and re.search("[A-Z]", data) != None
+    
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
