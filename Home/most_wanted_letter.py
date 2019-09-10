2467db7f6ea5c68b83ed0785f6259b21062009eb
eduardo costa <educostabra@gmail.com>
2019-09-10 15:54:41 -0300

    1st solution - removed comments

--- Home/most_wanted_letter.py
+++ Home/most_wanted_letter.py
@@ -1,26 +1,28 @@
-#/usr/bin/env checkio --domain=py run most-wanted-letter
+# /usr/bin/env checkio --domain=py run most-wanted-letter
 
 # You are given a text, which contains different english letters and punctuation symbols.    You should find the most frequent letter in the text. The letter returned must be in lower case.
 # While checking for the most wanted letter, casing does not matter, so for the purpose of your search,    "A" == "a".    Make sure you do not count punctuation symbols, digits and whitespaces, only letters.
-# 
+#
 # If you havetwo or more letters with the same frequency,    then return the letter which comes first in the latin alphabet.    For example -- "one" contains "o", "n", "e" only once for each, thus we choose "e".
-# 
+#
 # Input:A text for analysis as a string.
-# 
+#
 # Output:The most frequent letter in lower case as a string.
-# 
+#
 # Precondition:
 # A text contains only ASCII symbols.
 # 0 < len(text) ≤ 105
-# 
-# 
+#
+#
 # END_DESC
 #
 # Input: A text with letters, symbols and whitespaces.
-# Output: The most frequent letter in lower case OR the letters tha comes first in the alphabet in case of the same frequency. 
-# Constraints: 
+# Output: The most frequent letter in lower case OR the letters tha comes first in the alphabet in case of the same frequency.
+# Constraints:
+
+
 def checkio(text: str) -> str:
-    # algorithm 
+    # algorithm
     # Create variable mostFrequentyLetter
     # Create a hashtable <letter,frequency>
     # For each letter in the text
@@ -37,69 +39,48 @@ def checkio(text: str) -> str:
     #            mostFrequentLetter receives letter
     #    When there is no frequency for letter
     #     add letter to hashtable with frequency 1
-	#     if mostFrequentyLetter has no value
-	#	   mostFrequentyLetter receives letter
-	#     When there exists mostFrequentyLetter
-	#      Get the mFrequency from mostFrequentyLetter
-	#      If mFrequeny is 1 and letter is first in the alphabet than mostFrequentLetter then
-	#       mostFrequntyLetter receives letter
+    #         if mostFrequentyLetter has no value
+    #    	   mostFrequentyLetter receives letter
+    #         When there exists mostFrequentyLetter
+    #          Get the mFrequency from mostFrequentyLetter
+    #          If mFrequeny is 1 and letter is first in the alphabet than mostFrequentLetter then
+    #           mostFrequntyLetter receives letter
     # return mostFrequentLetter
-    
-    
-    # Create variable mostFrequentyLetter
-    mostFrequentyLetter = None
-    # Create a hashtable <letter,frequency>
-    hashTable = {}
-    # For each letter in the text
-    for letter in text:
-    #  Check if it is not a symbol
-        if ((letter.isalpha() or letter.isnumeric()) and not letter.isspace()):
-    #   Convert the letter to lowercase
-         letter = letter.lower()
-    #    Get the frequency for letter from hashtable
-         frequency = hashTable.get(letter)
-    #    If there is a frequency
-         if frequency:
-    #     Increment the frequency and update the hashtable
-          frequency += 1
-          hashTable[letter] = frequency
-    #     -> Get the mfrequency for mostFrequentLetter
-          mFrequency = hashTable[mostFrequentyLetter]
-    #     -> If frequency > mFrequency then
-          if (frequency > mFrequency):
-    #       -> mostFrequentyLetter receives letter
-            mostFrequentyLetter = letter
-    #        Else if frequency == mFrequency then
-          elif (frequency == mFrequency):
-    #        -> if letter is first in the alphabet than mostFrequentLetter then
-            if (ord(letter) < ord(mostFrequentyLetter)):
-    #            -> mostFrequentLetter receives letter
-                mostFrequentyLetter = letter
-    #    When there is no frequency for letter
-         else:
-    #    -> add letter to hashtable with frequency 1
-          hashTable[letter] = 1
-    #     -> if mostFrequentyLetter has no value 
-          if not mostFrequentyLetter:
-    #     -> mostFrequentyLetter receives letter
-           mostFrequentyLetter = letter
-    #     When exists mostFrequentyLetter       
-          else:
-    #         Get the mFrequency from mostFrequentyLetter
-              mFrequency = hashTable[mostFrequentyLetter]
-    #         If mFrequeny is 1 and letter is first in the alphabet than mostFrequentLetter then
-              if mFrequency == 1 and (ord(letter) < ord(mostFrequentyLetter)):
-    #             mostFrequntyLetter receives letter
-                  mostFrequentyLetter = letter
 
-    # return mostFrequentLetter
+    mostFrequentyLetter = None
+    frequencyTable = {}
+    for letter in text:
+        if (isLetter(letter)):
+            letter = letter.lower()
+            frequency = frequencyTable.get(letter)
+            if frequency:
+                frequencyTable[letter] = frequency+1
+                mFrequency = frequencyTable[mostFrequentyLetter]
+                if (frequency > mFrequency):
+                    mostFrequentyLetter = letter
+                elif (frequency == mFrequency):
+                    if (ord(letter) < ord(mostFrequentyLetter)):
+                        mostFrequentyLetter = letter
+            else:
+                frequencyTable[letter] = 1
+                if not mostFrequentyLetter:
+                    mostFrequentyLetter = letter
+                else:
+                    mFrequency = frequencyTable[mostFrequentyLetter]
+                    if mFrequency == 1 and (ord(letter) < ord(mostFrequentyLetter)):
+                        mostFrequentyLetter = letter
     return mostFrequentyLetter
+
+
+def isLetter(letter):
+    return (letter.isalpha() or letter.isnumeric()) and not letter.isspace()
+
 
 if __name__ == '__main__':
     print("Example:")
     print(checkio("Hello World!"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert checkio("Hello World!") == "l", "Hello test"
     assert checkio("How do you do?") == "o", "O is most wanted"
     assert checkio("One") == "e", "All letter only once."
