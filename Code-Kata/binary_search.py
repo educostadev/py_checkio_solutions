b59d844c0b0d4401af5e2fc7b520225b3f53b75d
Eduardo Costa <eduardo.costa@ciandt.com>
2022-12-28 14:03:24 -0300

    Bindary Search Fix

--- Code-Kata/binary_search.py
+++ Code-Kata/binary_search.py
@@ -1,25 +1,31 @@
 from typing import List, Any
 
-def solve(elements: List[Any], find: int) -> int:
+def solve(elements: List[Any], find) -> int:
+   
     lower_bound = 0
     upper_bound = len(elements)-1
-    while(lower_bound < upper_bound):
-        mid_index = (upper_bound - lower_bound) // 2
-        print(f"lower:{lower_bound} upper:{upper_bound} mid index:{mid_index}")
-        if (elements[mid_index] == find):
-            return mid_index
-        
-        if (elements[mid_index] > find):
-            upper_bound = mid_index-1
+    
+    while (lower_bound <= upper_bound):
+        mid = (upper_bound - lower_bound) // 2
+        mid = lower_bound + mid
+        value = elements[mid]
+        print(f"lower:{lower_bound} upper:{upper_bound} mid:{mid} value:{value}")
+        if (value == find):
+            return mid
+        if (value > find): 
+            upper_bound = mid-1 
+        if (value < find):
+            lower_bound = mid+1
 
-        if (elements[mid_index] < find):
-            lower_bound = mid_index+1
-           
     return -1
 
-if __name__ == '__main__':
-    
-    assert solve([1, 2, 3, 4, 5, 6, 7, 8], 2) == 1, "Failed"  
-    print("---")
+if __name__ == '__main__':    
+    print("⚪ [1]")
+    assert solve([1, 2, 3, 4, 5, 6, 7, 8], 2) == 1, "🔴 Failed"  
+    print("⚪ [2]")
+    assert solve([1, 2, 3, 4, 5, 6, 7, 8], 7) == 6, "🔴 Failed"  
+    print("⚪ [3]")
+    assert solve([1, 2, 3, 4, 5, 6, 7, 8], 8) == 7, "🔴 Failed"  
+    print("⚪ [4]")
     assert solve([1, 2, 3], 0) == -1, "Failed"  
-    print("Success!")
+    print("🟢 Success")
